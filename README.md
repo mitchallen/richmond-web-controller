0ca04f44ee09db3f3127116956599f2987bd4af8
mitchallen <mitch.c.allen@gmail.com>
2015-01-20 06:22:14 -0500

    Started making routes selectable

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Richmond Web Controller
 A Web controller for richmond.js
 -----------------------------------------------
 
+#### Version 0.0.3 release notes
+
+* Routes are now selectable via boolean setup
+
 #### Version 0.0.2 release notes
 
 * Fixed issue with params initialization
--- rwc.js
+++ rwc.js
@@ -8,22 +8,78 @@ var pkg = module.exports = {};
 pkg.name    = require("./package").name;
 pkg.version = require("./package").version;
 
-pkg.install = function( __app, __pkg, __router ) {
+var _parent = null;
+var _router = null;
+var _controller = [];
+
+pkg.parent = function( p ) {
+	_parent = p;
+	return this;
+}
+
+pkg.router = function( r ) {
+	_router = r;
+	return this;
+}
+
+var _getOne = false;
+
+pkg.getOne = function() {
+	_getOne = true;
+	return this;
+}
+
+var _getMany = false;
+
+pkg.getMany = function() {
+	_getMany = true;
+	return this;
+}
+
+var _post = false;
+
+pkg.post = function() {
+	_post = true;
+	return this;
+}
+
+var _del = false;
+
+pkg.del = function() {
+	_del = true;
+	return this;
+}
+
+var _put = false;
+
+pkg.put = function() {
+	_put = true;
+	return this;
+}
+
+var _patch = false;
+
+pkg.patch = function() {
+	_patch = true;
+	return this;
+}
+
+pkg.install = function( _app ) {
 	
-	var _params = require( './controllers/params' ).parent( __pkg );
+	var _params = require( './controllers/params' ).parent( _parent );
 	
-	__router.param( 'model', _params.model );
-	__router.param( 'id',    _params.id );
+	_router.param( 'model', _params.model );
+	_router.param( 'id',    _params.id );
 	
-	__app.use( 
-			__pkg.prefix(), 
-			[ 
-			    require( './controllers/get_one'   	)( __pkg, __router ), 
-			    require( './controllers/get_many'  	)( __pkg, __router ),
-			    require( './controllers/post'  		)( __pkg, __router ), 
-			    require( './controllers/del'   		)( __pkg, __router ), 
-			    require( './controllers/put'   		)( __pkg, __router ), 
-			    require( './controllers/patch' 		)( __pkg, __router ) 
-			]
+	if( _getOne  ) _controller.push( require( './controllers/get_one'   	)( _parent, _router ) )
+	if( _getMany ) _controller.push( require( './controllers/get_many'  	)( _parent, _router ) );
+	if( _post    ) _controller.push( require( './controllers/post'  		)( _parent, _router ) );
+	if( _del     ) _controller.push( require( './controllers/del'   		)( _parent, _router ) );
+	if( _put     ) _controller.push( require( './controllers/put'   		)( _parent, _router ) );
+	if( _put     ) _controller.push( require( './controllers/patch' 		)( _parent, _router ) );
+	
+	_app.use( 
+			_parent.prefix(), 
+			_controller
 		);
 }
