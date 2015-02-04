cdd3e8f60778f0e74ac2c31f2dd254460e2b2154
mitchallen <mitch.c.allen@gmail.com>
2015-02-03 20:51:52 -0500

    Ran put.js through jslint

--- controllers/put.js
+++ controllers/put.js
@@ -1,107 +1,99 @@
 /**
  * ./controllers/put.js
- * 
- * Usage:
- * 
- * The pkg var is from the parent module.
- * 
- * app.use( "/api", require( './controllers/put' )( parentInfo, methodOps ) );
- * 
- * See: http://www.restapitutorial.com/lessons/httpmethods.html
- * 
  */
 
-var _ssl = require( '../lib/ssl' ),
-	_rights = require( '../lib/rights' ),
-	u = require("underscore");
+"use strict";
 
-module.exports = function ( parentInfo, methodOps ) {
-	
-	var info = parentInfo || {};	
-	var parent = info.parent || {};
-	var log = parent.log;
-	var router = info.router;
-	var prefix = info.prefix;
-	// TODO - option for rightsAccess table later
-	
-	var methodOps  = methodOps || {};
-		
-	router.put( 
-			'/:model/:id', 
-			_ssl.isSSL( prefix, methodOps ),
-			_rights.isAuthorized( methodOps ), 
-			function( req, res, next ) {
-		var model = req.params.model;
-		var id    = req.params.id;  // Validated by params.id - but may not exist
-		collection = req.collection; // Set by router.param( 'model', ... );
-		// var collection = parent.model( model );
-		if( ! collection ) {
-			// TODO - should never get here if router.params did job right
-			var emsg = "INTERNAL ERROR: router.param let null model collection through.";
-			if( log ) log.error( emsg );
-			res.status(500).json( { error: emsg } );
-		} else {
-			if( ! id ) {
-				var emsg = "INTERNAL ERROR: router.id let null ID through";
-				if( log ) log.error( emsg );
-				res.status(500).json( { error: emsg } );
-				return;
-			} else {
-				
-				var obj = u.find( 
-						methodOps, 
-						function(obj) { return obj.model.toLowerCase() === model.toLowerCase() } );
-				var before = obj.before;
-				var after  = obj.after;
-				
-				function send() {
-					res.sendStatus(204);	// 204 - not returning data
-				}
-				
-				function update( body, options, _extras ) {
-					// TODO - does a merge - not very RESTful at the moment?
-					collection.update({
-						_id : req.params.id
-					}, {
-						$set : body
-					}, 
-					options, // options
-					function(err, numAffected) {
-						// numAffected is the number of updated documents
-						if (err) {
-							if( log ) log.error( err );
-							return res.status(403).json( { error: err.message } );
-						} else {
-							if( after ) {
-								after( 
-									{ 
-										req: req, 
-										res: res,
-										numAffected: numAffected,
-										extras: _extras }, 
-									send );
-							} else {
-								send();
-							}
-						}
-					});
-				}
-				
-				if( before ) {
-					before(  
-						{ req: req }, 
-						update );
-				} else {
-					// TODO - look at options (second param)
-					update( req.body, {}, null );
-				}
-			}
-		}
-	});
-	
-	// NOTE: We are returning the router here.
-	
-	return router;
+/*jslint unparam: true, nomen: true*/
+
+var m_ssl = require('../lib/ssl'),
+    m_rights = require('../lib/rights'),
+    u = require("underscore");
+
+module.exports = function (parentInfo, mOps) {
+    var info = parentInfo || {},
+        parent = info.parent || {},
+        log = parent.log,
+        router = info.router,
+        prefix = info.prefix,
+        methodOps  = mOps || {};
+    router.put(
+        '/:model/:id',
+        m_ssl.isSSL(prefix, methodOps),
+        m_rights.isAuthorized(methodOps),
+        function (req, res, next) {
+            var model = req.params.model,
+                emsg = null,
+                obj = null,
+                before = null,
+                after = null,
+                id    = req.params.id,  // Validated by params.id - but may not exist
+                collection = req.collection; // Set by router.param('model', ...);
+            if (!collection) {
+                // Should never get here if router.params did job right
+                emsg = "INTERNAL ERROR: router.param let null model collection through.";
+                if (log) { log.error(emsg); }
+                res.status(500).json({ error: emsg });
+                return;
+            }
+            if (!id) {
+                emsg = "INTERNAL ERROR: router.id let null ID through";
+                if (log) { log.error(emsg); }
+                res.status(500).json({ error: emsg });
+                return;
+            }
+            obj = u.find(
+                methodOps,
+                function (obj) { return obj.model.toLowerCase() === model.toLowerCase(); }
+            );
+            before = obj.before;
+            after  = obj.after;
+            function send() {
+                res.sendStatus(204);    // 204 - not returning data
+            }
+            function update(body, options, _extras) {
+                    // Does a merge - not very RESTful?
+                collection.update(
+                    {
+                        _id : req.params.id
+                    },
+                    {
+                        $set : body
+                    },
+                    options, // options
+                    function (err, numAffected) {
+                        // numAffected is the number of updated documents
+                        if (err) {
+                            if (log) {
+                                log.error(err);
+                            }
+                            return res.status(403).json({ error: err.message });
+                        }
+                        if (after) {
+                            after(
+                                {
+                                    req: req,
+                                    res: res,
+                                    numAffected: numAffected,
+                                    extras: _extras
+                                },
+                                send
+                            );
+                        } else {
+                            send();
+                        }
+                    }
+                );
+            }
+            if (before) {
+                before(
+                    { req: req },
+                    update
+                );
+            } else {
+                update(req.body, {}, null);
+            }
+        }
+    );
+    return router;
 };
-
-
