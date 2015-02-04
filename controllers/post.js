9145f82dbdcad0a8fc65743fb5f0b513394c228b
mitchallen <mitch.c.allen@gmail.com>
2015-02-03 20:24:50 -0500

    Ran post.js through jslint

--- controllers/post.js
+++ controllers/post.js
@@ -1,126 +1,108 @@
 /**
- * ./controllers/post.js
- * 
- * Usage:
- * 
- * The pkg var is from the parent module.
- * 
- * app.use( "/api", require( './controllers/post' )( parentInfo, methodOps ) );
- * 
- * See: http://www.restapitutorial.com/lessons/httpmethods.html
- * 
+ * ./controllers/post.js 
  */
 
-var _ssl = require( '../lib/ssl' ),
-	_rights = require( '../lib/rights' ),
-	u = require("underscore");
+"use strict";
 
-module.exports = function ( parentInfo, methodOps ) {
-	
-	var info = parentInfo || {};	
-	var parent = info.parent || {};
-	var log = parent.log;
-	var router = info.router;
-	var prefix = info.prefix;
-	// TODO - option for rightsAccess table later
-	
-	var methodOps  = methodOps || {};
-		
-	var _routeName = "post";
-	
-	router.post( 
-			'/:model', 
-			_ssl.isSSL( prefix, methodOps ),
-			_rights.isAuthorized( methodOps ),
-			function( req, res, next ) {
-				
-		model = req.params.model;
-		// var collection = parent.model( model );
-		var collection = req.collection; // Set by router.param( 'model', ... );
-		if( ! collection ) {
-			// TODO - should never get here if router.params did job right
-			var emsg = "INTERNAL ERROR: router.param let null model collection through.";
-			if( log ) {
-			     log.error( emsg );
-			} else {
-			     console.error(emsg);
-			}
-			res.status(500).json( { error: emsg } );
-			return;
-		} else {
-			
-			var obj = u.find( 
-					methodOps, 
-					function(obj) { return obj.model.toLowerCase() === model.toLowerCase() } );
-			var before = obj.before;
-			var after  = obj.after;
-			
-			function sendDocument( doc ) {
-				// log.info( "### POST: sendDocument");
-				res
-					.location( "/" + model + "/" + doc._id )
-					.status(201)	// Created
-					.json( doc );	// TODO - this even returns select: false fields
-			}
-			
-			function saveDocument( body, _extras ) {
-				// log.info( "DEBUG: Save Document")
-				var record = new collection( body );
-				if( ! record ) {
-					var emsg = "ERROR: Creating new " + model;
-					if( log ) { 
-					   log.error( emsg );
-					} else {
-					   console.error(emsg);
-					}
-					res.status(403).json( { error: emsg } );
-					return;
-				} else {
-					// log.error( "### POST: ABOUT TO SAVE: " + JSON.stringify( body ) );
-					record.save( function( err, doc ) {
-						if( err ) { 
-							var emsg = "ERROR: Can't create new '" + model + "'";
-							// Typical - doesn't pass validation, etc.
-							var ex = { error: emsg, message: err.message }
-							if( log ) {
-							     log.error( emsg );
-							     log.error( ex );
-							} else {
-							     console.error( emsg );
-							     console.error( ex );
-							}
-							res.status(403).json( ex ); 
-						} else {
-							// log.error( "### POST: setting up AFTER");
-							if( after ) {
-								after( 
-										{ 
-											req: req,
-											res: res,
-											result: doc, 
-											extras: _extras }, 
-										sendDocument );
-							} else {
-								// log.info( "### POST: skipping AFTER, just calling sendDocument")
-								sendDocument( doc );
-							}
-						}
-					});
-				}
-			}
-			
-			if( before ) {
-				before( 
-						{ req: req }, 
-						saveDocument )
-			} else {
-				// log.info( "### POST: skipping BEFORE, just calling saveDocument");
-				saveDocument( req.body, null );
-			}
-		}
-	});
-		
-	// Note that we are returning the router here.
-	
-	return router;
+/*jslint unparam: true, nomen: true*/
+
+var m_ssl = require('../lib/ssl'),
+    m_rights = require('../lib/rights'),
+    u = require("underscore");
+
+module.exports = function (parentInfo, mOps) {
+    var info = parentInfo || {},
+        parent = info.parent || {},
+        log = parent.log,
+        router = info.router,
+        prefix = info.prefix,
+        methodOps  = mOps || {};
+    router.post(
+        '/:model',
+        m_ssl.isSSL(prefix, methodOps),
+        m_rights.isAuthorized(methodOps),
+        function (req, res, next) {
+            var model = req.params.model,
+                emsg = "",
+                ex = null,
+                obj = null,
+                before = null,
+                after = null,
+                Collection = req.collection;// Set by router.param('model', ...);
+            if (!Collection) {
+                // Should never get here if router.params did job right
+                emsg = "INTERNAL ERROR: router.param let null model collection through.";
+                if (log) {
+                    log.error(emsg);
+                } else {
+                    console.error(emsg);
+                }
+                res.status(500).json({ error: emsg });
+                return;
+            }
+            obj = u.find(
+                methodOps,
+                function (obj) { return obj.model.toLowerCase() === model.toLowerCase(); }
+            );
+            before = obj.before;
+            after  = obj.after;
+            function sendDocument(doc) {
+                res
+                    .location("/" + model + "/" + doc._id)
+                    .status(201)    // Created
+                    .json(doc);
+            }
+            function saveDocument(body, _extras) {
+                var record = new Collection(body);
+                if (!record) {
+                    emsg = "ERROR: Creating new " + model;
+                    if (log) {
+                        log.error(emsg);
+                    } else {
+                        console.error(emsg);
+                    }
+                    res.status(403).json({ error: emsg });
+                    return;
+                }
+                record.save(function (err, doc) {
+                    if (err) {
+                        emsg = "ERROR: Can't create new '" + model + "'";
+                        // Typical - doesn't pass validation, etc.
+                        ex = { error: emsg, message: err.message };
+                        if (log) {
+                            log.error(emsg);
+                            log.error(ex);
+                        } else {
+                            console.error(emsg);
+                            console.error(ex);
+                        }
+                        res.status(403).json(ex);
+                        return;
+                    }
+                    if (after) {
+                        after(
+                            {
+                                req: req,
+                                res: res,
+                                result: doc,
+                                extras: _extras
+                            },
+                            sendDocument
+                        );
+                    } else {
+                        sendDocument(doc);
+                    }
+                });
+            }
+            if (before) {
+                before(
+                    { req: req },
+                    saveDocument
+                );
+            } else {
+                saveDocument(req.body, null);
+            }
+        }
+    );
+    return router;
 };
